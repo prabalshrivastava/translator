3dd58573ca815949e57b8447ab5acce86574a054
Prabal Shrivastava <prabalshrivastva54@gmail.com>
2019-03-09 13:08:49 +0530

    language detection added

--- popup.js
+++ popup.js
@@ -1,13 +1,31 @@
-    var languageSelector = document.querySelector("#languageSelector");
-    languageSelector.addEventListener('change', function() {
-        let params = {
-            active : true,
-            currentWindow : true
-        }
-        chrome.tabs.query(params,gotTabs);
-    });
-    function gotTabs(tabs){
-        var currentLanguage = languageSelector.value; 
-        console.log(tabs,"currentLanguage : " + currentLanguage);
-        chrome.tabs.sendMessage(tabs[0].id,currentLanguage);
-    }
+var languageSelector = document.querySelector("#languageSelector");
+let params = {
+    active : true,
+    currentWindow : true
+}
+
+window.onload=function(){
+    chrome.tabs.query(params,sendDetectLangRequest);
+};
+function sendDetectLangRequest(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id,'detectMyLang');
+}
+
+languageSelector.addEventListener('change', function() {
+    chrome.tabs.query(params,gotTabs);
+});
+function gotTabs(tabs){
+    var currentLanguage = languageSelector.value; 
+    console.log(tabs,"currentLanguage : " + currentLanguage);
+    chrome.tabs.sendMessage(tabs[0].id,currentLanguage);
+}
+
+
+
+chrome.runtime.onMessage.addListener(displayCurrentLanguage);
+
+function displayCurrentLanguage(message,sender,sendResponse){
+    console.log("inside displayCurrentLanguage",message);
+    var currLangElement = document.querySelector('#currentLanguage');
+    currLangElement.innerHTML="The Current Language is : "  + message.data;
+}
